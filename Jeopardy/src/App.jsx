845a0aae8efaf59eee497f363c73e66cceaa3f46
Kyle Pixley <kyle_pixley@hotmail.com>
2025-11-08 15:49:19 -0500

    Intro Animations/WIP

--- Jeopardy/src/App.jsx
+++ Jeopardy/src/App.jsx
@@ -1,40 +1,48 @@
-import { useState } from 'react';
-import './App.css';
+import { useState, useEffect } from 'react';
+import GameOne from './Components/GameOne';
 
 function App() {
 
   const [ catagories, setCatagories ] = useState([
 
   ])
+  const [ gameStart, setGameStart ] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setGameStart(true);
+    }, 3450)
+  })
 
   return (
-    <div className='flex  justify-center h-full'>
+    <div className='flex  justify-center h-full bg-blue-600'>
       <section>
-          <span className='inline-block text-blue-600 text-[7em] text-shadow-lg/100 
+          <span className='inline-block text-white text-[7em] text-shadow-lg/100 
             animate-slide-in-right'>
               J</span>
-          <span className='inline-block text-blue-600 text-[7em] text-shadow-lg/100 
+          <span className='inline-block text-white text-[7em] text-shadow-lg/100 
             animate-slide-in-right-2'>
               E</span>
-          <span className='inline-block text-blue-600 text-[7em] text-shadow-lg/100 
+          <span className='inline-block text-white text-[7em] text-shadow-lg/100 
             animate-slide-in-right-3'>
               O</span>
-          <span className='inline-block text-blue-600 text-[7em] text-shadow-lg/100 
+          <span className='inline-block text-white text-[7em] text-shadow-lg/100 
             animate-slide-in-right-4'>
               P</span>
-          <span className='inline-block text-blue-600 text-[7em] text-shadow-lg/100 
+          <span className='inline-block text-white text-[7em] text-shadow-lg/100 
             animate-slide-in-right-5'>
               A</span>
-          <span className='inline-block text-blue-600 text-[7em] text-shadow-lg/100 
+          <span className='inline-block text-white text-[7em] text-shadow-lg/100 
             animate-slide-in-right-6'>
               R</span>
-          <span className='inline-block text-blue-600 text-[7em] text-shadow-lg/100 
+          <span className='inline-block text-white text-[7em] text-shadow-lg/100 
             animate-slide-in-right-7'>
               D</span>
-          <span className='inline-block text-blue-600 text-[7em] text-shadow-lg/100 
+          <span className='inline-block text-white text-[7em] text-shadow-lg/100 
             animate-slide-in-right-8'>
               Y</span>
       </section>
+      { gameStart ? <GameOne /> : null }
     </div>
   )
 }
---
+++ Jeopardy/src/Components/GameOne.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function GameOne() {
+  return (
+    <div className='position: absolute h-full w-full animate-slide-down flex flex-col justify-center items-center'>
+        <section className='w-[80%] h-[80%] bg-white'>
+            Jeopardy
+        </section>
+    </div>
+  )
+}
+
+export default GameOne;
